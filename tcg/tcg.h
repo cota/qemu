d564077260d242d984694923686f3bc86af9b905
Emilio G. Cota <cota@braap.org>
2017-07-16 15:13:52 -0400

    tcg: check CF_PARALLEL instead of parallel_cpus
    Thereby decoupling the resulting translated code from the current state
    of the system.
    
    The tb->cflags field is not passed to tcg generation functions. So
    we add a field to TCGContext, storing there a copy of tb->cflags.
    
    Most architectures have <= 32 registers, which results in a 4-byte hole
    in TCGContext. Use this hole for the new field.
    
    Reviewed-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- tcg/tcg.h
+++ tcg/tcg.h
@@ -656,6 +656,7 @@ struct TCGContext {
     uintptr_t *tb_jmp_target_addr; /* tb->jmp_target_addr if !USE_DIRECT_JUMP */
 
     TCGRegSet reserved_regs;
+    uint32_t tb_cflags; /* cflags of the current TB */
     intptr_t current_frame_offset;
     intptr_t frame_start;
     intptr_t frame_end;
